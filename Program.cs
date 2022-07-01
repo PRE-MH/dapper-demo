c3a0d2849d81e3ca86078374b35e4e75bd3246fb
Mohamed Hakim <midohakim2@gmail.com>
2022-07-01 13:49:13 +0200

    connexion à la bd

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Data.Sqlite;
+
+using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+{
+    connection.Open();
+
+    var command = connection.CreateCommand();
+    command.CommandText =
+    @"
+        SELECT *
+        FROM dog
+    ";
+    using (var reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            var value = reader.GetString(0);
+            Console.WriteLine($"Hello, {value}!");
+        }
+    }
+}

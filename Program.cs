1320a6751372997b7b4aa595f71b94e087e31f39
Mohamed Hakim <midohakim2@gmail.com>
2022-07-01 16:22:23 +0200

    Update + Delete + Inssert

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using Dapper;
 using Microsoft.Data.Sqlite;
+/**using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+{
+    connection.Open();
 
+    connection.Execute("INSERT INTO Dog VALUES (20, 3, 'dog3', 16)");
+}**/
+/**using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+{
+    connection.Open();
+
+    connection.Execute("DELETE FROM Dog WHERE Id=3");
+}**/
+using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+{
+    connection.Open();
+
+    connection.Execute("UPDATE Dog SET Weight=100 WHERE Id=1");
+}
 using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
 {
     connection.Open();
 
     var sql = "SELECT * FROM Dog";
-    var result = connection.Query<Dog>(sql);
+    var result = connection.Query<Dog1>(sql);
 
     foreach(var dog in result)
     {
@@ -15,7 +32,7 @@ using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
 }
 
 
-class Dog
+class Dog1
 {
     public string Name {get; set;}
 
@@ -23,8 +40,10 @@ class Dog
 
     public int Id {get; set;}
 
+    public double Weight{get;set;}
+
     public override string ToString()
     {
-        return $"DOG {Id}: {Name} {Age}";
+        return $"DOG {Id}: Name {Name} | Age {Age} | Weight {Weight} ";
     }
 }

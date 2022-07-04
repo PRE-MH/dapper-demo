7e3537dd1d948941dcc199efdfdee79b9a1f0bc5
Mohamed Hakim <midohakim2@gmail.com>
2022-07-04 15:28:28 +0200

    using 2 classes

--- Program.cs
+++ Program.cs
@@ -1,52 +1,69 @@
 ﻿using Dapper;
 using Microsoft.Data.Sqlite;
-using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+using (var connection = new SqliteConnection("Data Source=./data/demo1.db"))
 {
-    connection.Open();
-    int age=16;
-    string name="dog2";
-    double weight=15.3;
-
-    connection.Execute("INSERT INTO Dog (Age, Name, Weight) VALUES (@age ,@name ,@weight )", new { age, name, weight});
+    var sql =
+    @"select * from Post p
+    left join User u on u.Id = p.OwnerId
+    Order by p.Id";
+    
+    var data = connection.Query<Post, User, Post>(sql, (post, user) => { post.Owner = user; return post;});
+    
+    foreach(var post in data){
+    
+        Console.WriteLine(post);
+    
+    }
 }
 /**using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
 {
     connection.Open();
-
+    int age=16;
+    string name="dog1";
+    double weight=15.3;
+    connection.Execute("INSERT INTO Dog (Age, Name, Weight) VALUES (@age ,@name ,@weight )", new { age, name, weight});
+}**/
+/**using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+{
+    connection.Open();
     connection.Execute("DELETE FROM Dog WHERE Id=3");
 }**/
 /**using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
 {
     connection.Open();
-
     connection.Execute("UPDATE Dog SET Weight=100 WHERE Id=1");
 }**/
-using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
+/**using (var connection = new SqliteConnection("Data Source=./data/demo.db"))
 {
     connection.Open();
-
     var sql = "SELECT * FROM Dog";
-    var result = connection.Query<Dog1>(sql);
-
+    var result = connection.Query<Dog>(sql);
     foreach(var dog in result)
     {
         Console.WriteLine(dog);
     }
-}
-
-
-class Dog1
+}*/
+class Post
 {
-    public string Name {get; set;}
-
-    public int Age {get; set;}
-
-    public int Id {get; set;}
-
-    public double Weight{get;set;}
-
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public User Owner { get; set; }
+    public int OwnerId { get; set ;}
+    
     public override string ToString()
     {
-        return $"DOG {Id}: Name {Name} | Age {Age} | Weight {Weight} ";
+    
+        return $"Post {Id}= Title: {Title} | Owner: {Owner.Name} | Content: {Content} | OwnerId: {OwnerId} ";
+    
+    }
+}
+class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public override string ToString(){
+        return $"Name: {Name} | Id: {Id} ";
     }
 }
